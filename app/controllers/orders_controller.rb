1dc9364476397f807e7499d4e232f3a1e0288fa2
miyasan-09 <145628704+miyasan-09@users.noreply.github.com>
2023-10-26 22:32:29 +0900

    orderテーブルshipテーブル作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+
+
+
+
+
+end
